7f1071f05d3404c6d0dad9029c2edaaebc1267c4
jokaproduct <jokaproduct@gmail.com>
2023-06-18 22:20:14 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+let text = " lex luter has A big suit "
+
+
+console. log(
+
+  text.slice(text.search('A'), text.search('A') +1 ) + 
+  text.slice(text.search('lex'), text.search('lex') +3) 
+
+); 
