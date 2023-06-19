cd319c9852d268beb8ba2fd3975def4d3fcb1f9e
jokaproduct <135736920+jokaproduct@users.noreply.github.com>
2023-06-19 15:43:03 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,7 @@
-let text = " lex luter has A big suit "
+let month = prompt('Кокое сейчас время сезона?')
 
-
-console. log(
-
-  text.slice(text.search('A'), text.search('A') +1 ) + 
-  text.slice(text.search('lex'), text.search('lex') +3) 
-
-); 
+if (month == '3' || month == '4' || month == '5') { alert("Весна") }
+else if (month == '6' || month == '7' || month == '8') { alert("Лето") }
+else if (month == '9' || month == '10' || month == '11') { alert("Kuz") }
+else if (month == '12' || month == '1' || month == '2') { alert("Зима") }
+else {alert ("ошибка")} 
